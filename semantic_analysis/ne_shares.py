0f716f1ab518a1acaca7063a24359d1b242c179d
julianschroeter <julian.schroeter@gmx.de>
2022-03-18 15:37:22 +0100

    commit Feb 9 22

---
+++ semantic_analysis/ne_shares.py
@@ -0,0 +1,63 @@
+
+def calculate_share(text_as_list, ne_list, normalize='l1', standardize=False, case_sensitive=False):
+    """
+    proceeds the calculation operation and stores the result wordlist_shares attribute.
+    """
+    # text_as_list = text.split(" ")
+
+
+    hits = 0
+    if case_sensitive == True:
+        for token in text_as_list:
+            if token in ne_list:
+                hits += 1
+    elif case_sensitive == False:
+        wordlist_lower = [token.lower() for token in ne_list]
+        for token in text_as_list:
+            if token.lower() in wordlist_lower:
+                hits += 1
+
+    if normalize ==  "abs" and standardize == False:
+        share = hits
+    elif normalize == "l1" and standardize == False:
+        share = hits / len(text_as_list)
+    elif normalize == "l1" and standardize == True:
+        share = hits / (len(text_as_list) * len(ne_list))
+
+
+    return share
+
+
+import pandas as pd
+import os
+from preprocessing.presetting import global_corpus_representation_directory, vocab_lists_dicts_directory, load_stoplist
+
+system = "wcph113"
+
+list_path = os.path.join(vocab_lists_dicts_directory(system), "toponym_lists", "italian.txt")
+italian_list = load_stoplist(list_path)
+
+list_path = os.path.join(vocab_lists_dicts_directory(system), "toponym_lists", "french.txt")
+french_list = load_stoplist(list_path)
+
+list_path = os.path.join(vocab_lists_dicts_directory(system), "toponym_lists", "spanish.txt")
+spanish_list = load_stoplist(list_path)
+
+
+
+# italian_list = ["Eckbert"]
+
+df_infilepath = os.path.join(global_corpus_representation_directory(system), "NEs_document_Matrix_test.csv")
+df = pd.read_csv(df_infilepath, index_col=0)
+
+df_outfilepath = os.path.join(global_corpus_representation_directory(system), "toponym_share_Matrix.csv")
+
+print(df)
+
+df["it_top"] = df["Orte"].apply(lambda x: calculate_share(str(x).split(". "), ne_list=italian_list))
+df["fr_top"] = df["Orte"].apply(lambda x: calculate_share(str(x).split(". "), ne_list=french_list))
+df["sp_top"] = df["Orte"].apply(lambda x: calculate_share(str(x).split(". "), ne_list=spanish_list))
+
+df = (df[["it_top", "fr_top", "sp_top"]])
+
+df.to_csv(path_or_buf=df_outfilepath)
